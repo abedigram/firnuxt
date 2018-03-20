25647a430a00c60a4d0e86a1e651ba2f8ceaf533
unknown <mbapost@yahoo.com>
2018-03-20 17:23:18 +0330

    fir hello world commit

---
+++ nuxt.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+  head: {
+    title: 'secprimenuxt',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      {  }
+    ]
+  },
+
+  // build: {
+  //   extend (config, { isDev, isClient }) {
+  //     if (isDev && isClient) {
+  //       config.module.rules.push({
+  //         enforce: 'pre',
+  //         test: /\.(js|vue)$/,
+  //         loader: 'eslint-loader',
+  //         exclude: /(node_modules)/
+  //       })
+  //     }
+  //   }
+  // }
+
+}
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>secprimenuxt</h1>
+</template>
